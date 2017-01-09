dd437c94e0ec3034a031f07826336e99ae29ba44
bill bittner <bittner.w@gmail.com>
2017-01-09 14:51:10 -0800

    updated home.html

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,7 +1,12 @@
 //api routes
 
 app.get("/api/friends", function(request, response){
-    //send json of friend array
+    //display json of friend array
 })
 
-app.post("/api/friends", function())
+app.post("/api/friends", function(){
+    //take in form submisssions
+    //compare the form submission to the friends already in the array
+    //display a modal with the closest match
+    //add the form submission to the friends array
+})
